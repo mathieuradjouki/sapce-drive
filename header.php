f0d7ff57041d6612eaffbc67c4a4baa9b19209d8
mathieuradjouki <m.radjouki@codeur.online>
2020-09-11 11:41:47 +0200

    add top section and fixe header

--- header.php
+++ header.php
@@ -8,72 +8,110 @@
     <title>Space Drive</title>
   </head>
   <body>
-    <header>
-      <div class="top-nav">
+    <section  class="top-section">
+      <header>
+        <!-- First navbar -->
+        <div class="top-nav">
+          <div class="container">
+            <div class="nav pre-nav">
+              <ul class="nav info">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">support@hostcox.com</a>
+                </li>
+                <li class="nav-link">|</li>
+                <li class="nav-link">+44 7700 900077</li>
+              </ul>
+              <ul class="nav nav-log">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Live Chat</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Support</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Second navbar -->
         <div class="container">
-          <div class="nav pre-nav">
+          <div class="nav main-nav">
             <ul class="nav info">
               <li class="nav-item">
-                <a class="nav-link" href="#">support@hostcox.com</a>
+                <a class="navbar-brand nav-link" href="#">SpaceDrive</a>
               </li>
-              <li class="nav-link">|</li>
-              <li class="nav-link">+44 7700 900077</li>
             </ul>
             <ul class="nav nav-log">
               <li class="nav-item">
-                <a class="nav-link" href="#">Live Chat</a>
+                <a class="nav-link" href="#">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Login</a>
+                <a class="nav-link" href="#">Domain</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Support</a>
+                <a class="nav-link" href="#">Hosting</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">WHMCS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
               </li>
             </ul>
           </div>
         </div>
-      </div>
-      <div class="container">
-        <div class="nav main-nav">
-          <ul class="nav info">
-            <li class="nav-item">
-              <a class="navbar-brand nav-link" href="#">SpaceDrive</a>
-            </li>
-          </ul>
-          <ul class="nav nav-log">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Domain</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Hosting</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">WHMCS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="header-contain">
-        <div class="container">
-          <div class="row row-header-contain">
-          <div class="buy-now">
-            <h1>Powerful  Hosting Service Provider</h1>
-            <p>Millions of customers rely on our products to get their ideas online.</p>
-            <button type="button" name="button" class="btn buy-now-btn">BUY NOW</button>
+  <!-- header contain -->
+        <div class="header-contain">
+          <div class="container">
+            <div class="row row-header-contain">
+              <div class="buy-now">
+                <h1>Powerful  Hosting Service Provider</h1>
+                <p>Millions of customers rely on our products to get their ideas online.</p>
+                <button type="button" name="button" class="btn buy-now-btn">BUY NOW</button>
+              </div>
+                <img src="images/header-img.png" alt="" class="header-img">
+            </div>
           </div>
-
-            <img src="images/header-img.png" alt="" class="header-img">
-
         </div>
+      </header>
+      <div class="search-domain">
+        <div class="container">
+          <h2>Search Your Domain</h2>
+          <form class="" action="index.php" method="post">
+            <input type="text" placeholder="Search your domain" name="search" class="input-domain">
+              <button type="submit" name="button" class="domain-btn">SEARCH</button>
+          </form>
+          <p>.com $9.99 .NET $7.54 .ORG  $7.54 .CO $7.54 .UK $7.54</p>
         </div>
       </div>
-    </header>
+      <div class="top-service container">
+        <!-- <div class="container"> -->
+          <div class="card" style="width: 18rem;">
+            <!-- <img src="images/seo1.png" class="card-img-top" alt="..."> -->
+            <div class="card-body text-center">
+              <h5 class="card-title">Free update</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          <div class="card" style="width: 18rem;">
+            <!-- <img src="images/seo2.png" class="card-img-top" alt="..."> -->
+            <div class="card-body text-center">
+              <h5 class="card-title">Data Security</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          <div class="card" style="width: 18rem;">
+            <!-- <img src="images/seo3.png" class="card-img-top" alt="..."> -->
+            <div class="card-body text-center">
+              <h5 class="card-title">Cloud backup</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        <!-- </div> -->
+      </div>
+    </section>
