12374e4c3557f0d57fe3a61338c08f359267b9a7
mathieuradjouki <m.radjouki@codeur.online>
2020-09-17 12:00:48 +0200

    commit before lunch

--- secure.php
+++ secure.php
@@ -15,5 +15,20 @@
         <img src="images/sercure-service.png" alt="">
       </div>
     </div>
+    <div class="secure-domain">
+      <div class="secure-domain-img">
+        <img src="images/secure-domain.png" alt="">
+      </div>
+      <div class="secure-domain-text">
+        <h4>Secure domain registration</h4>
+        <p>Dedicated Hosts. An Amazon EC2 Dedicated Host is a physical server with EC2 instance capacity fully dedicated to your use. Dedicated Hosts allow you to use your existing per-socket, per-core,...</p>
+        <ul>
+          <li><img src="images/checked.png" alt="">Web Asset Delivery</li>
+          <li><img src="images/checked.png" alt="">24/7 Dedicated Support</li>
+          <li><img src="images/checked.png" alt="">Global Datacenters</li>
+        </ul>
+        <a href="#">Read more...</a>
+      </div>
+    </div>
   </div>
 </div>
