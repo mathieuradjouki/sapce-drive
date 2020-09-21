635082b299f414e1ca55c4649f3e73745a5e4acd
mathieuradjouki <m.radjouki@codeur.online>
2020-09-21 10:36:24 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,56 @@
+    <footer>
+      <div class="items">
+        <div class="space-drive">
+          <h5>Space Drive</h5>
+          <ul>
+            <li>4615 N. University Drive,</li>
+            <li>Coral Springs, FL 33067</li>
+            <li>(800)674-2959</li>
+          </ul>
+        </div>
+        <div class="quick-links">
+          <h5>Quick links</h5>
+          <ul>
+            <li>Domain</li>
+            <li>Hosting</li>
+            <li>WHMCS</li>
+            <li>Blog</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div class="Support">
+          <h5>Support</h5>
+          <ul>
+            <li>Product</li>
+            <li>Community</li>
+            <li>Report Abuse</li>
+          </ul>
+        </div>
+        <div class="account">
+          <h5>Account</h5>
+          <ul>
+            <li>My Account</li>
+            <li>My Renewals</li>
+            <li>Create Account</li>
+          </ul>
+        </div>
+        <div class="newsletter">
+          <h5>News letter Sign-up</h5>
+          <form class="email" action="index.php" method="post">
+            <input type="email" name="email" placeholder="Email Address" value="">
+            <button type="submit" name="email" class="submit-footer">Submit</button>
+          </form>
+        </div>
+      </div>
+      <img src="images/line.png" alt="line">
+      <div class="copyright">
+        <p>© 2019 spacedrive.inc</p>
+        <div class="conditions">
+          <a href="#">Privacy Policy</a>
+          <a href="#">Terms &Conditions</a>
+        </div>      
+      </div>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
     <script type="text/javascript" src="main.js"></script>
