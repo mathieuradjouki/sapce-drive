9d4c2de2803a80122e5e9f40cbd945d547e6e3a9
mathieuradjouki <m.radjouki@codeur.online>
2020-09-29 11:57:56 +0200

    modification fonction add

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var add = document.getElementById("add");
 var newServices = document.getElementById("FormNewNosServices");
 
-add.addEventListener("click", changeDisplay);
-
-function changeDisplay(){
-  newServices.style.display = "block";
+add.addEventListener("click",(e)=>{changeDisplay(newServices)});
+// La fonction {changeDisplay(newServices)} est placé dans une fonction anonyme function(e)
+function changeDisplay(e){
+  e.style.display = "block";
 }
